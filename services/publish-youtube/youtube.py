f433a145c1e7e0d22c6bca072100d93c67bf02a5
Wallace <rwwalla2@illinois.edu>
2019-04-10 16:09:52 -0500

    youtube service

---
+++ services/publish-youtube/youtube.py
@@ -0,0 +1,33 @@
+import os
+import json
+import requests
+from flask import Flask, request, jsonify
+
+'''
+Flask app that takes in zipped 360 video file, JSON data, and credentials 
+and publishes to YouTube360. 
+
+'''
+
+app = Flask(__name__)
+
+@app.route('/youtube', methods=['POST'])
+def youtube():
+    if request.method == 'POST':
+        # build post request to YouTube
+        
+        post_data = json.loads(request.form.get('data'))
+        token = post_data.get('token')
+        headers = {'Authorization': 'Token {}'.format(token)}
+        
+        data = {'name': post_data.get('name')}
+
+        file = request.files['file']
+        file.save('model.zip')
+        
+
+        return jsonify({'message': 'post received'})
+
+
+if __name__ == '__main__':
+    app.run()

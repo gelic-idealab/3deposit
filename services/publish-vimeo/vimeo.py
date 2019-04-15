2dc97722f3c02bd858b2f35c0fe14932dfedb2f8
Wallace <rwwalla2@illinois.edu>
2019-04-15 17:06:39 -0500

    unpack request & construct post to vimeo

--- services/publish-vimeo/vimeo.py
+++ services/publish-vimeo/vimeo.py
@@ -5,32 +5,46 @@ import vimeo
 from flask import Flask, request, jsonify
 
 '''
-Flask app that takes in zipped 360 video file, JSON data, and credentials 
-and publishes to YouTube360. 
+Flask app that takes in 360 video file, JSON data, and credentials 
+and publishes to Vimeo. 
 
 '''
 
 app = Flask(__name__)
 
 @app.route('/vimeo', methods=['POST'])
-def youtube():
+def post_to_vimeo():
     if request.method == 'POST':
-        # build post request to YouTube
-        # see https://github.com/youtube/api-samples/blob/master/python/upload_video.py
-        # pip install --upgrade google-auth google-auth-oauthlib google-auth-httplib2 
-
+        # unpack request
         post_data = json.loads(request.form.get('data'))
+
         auth = post_data.get('auth')
-        token = auth.get('apitoken')
-        # headers = {'Authorization': 'Token {}'.format(token)}
-
+        access_token = auth.get('token')
+        client_id = auth.get('clientid')
+        client_secret = auth.get('clientsecret')
+        
         metadata = post_data.get('metadata')
-        filename = metadata.get('filename')
+        name = metadata.get('name')
+        description = metadata.get('description')
 
+        filename = metadata.get('filename')
         file = request.files['file']
         file.save(filename)
 
-        return jsonify({'message': token})
+        # construct request
+        client = vimeo.VimeoClient(
+        token=access_token,
+        key=client_id,
+        secret=client_secret
+        )
+
+        file_name = filename
+        uri = client.upload(file_name, data={
+        'name': name,
+        'description': description
+        })
+
+        return jsonify({'uri': uri})
 
 if __name__ == '__main__':
     app.run()

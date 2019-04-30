9f5c46586bfdddac4c8b45889c30205cd405f92b
Wallace <rwwalla2@illinois.edu>
2019-04-30 12:54:37 -0500

    flask wrapper

--- services/store-minio/minio-service.py
+++ services/store-minio/minio-service.py
@@ -1,16 +1,35 @@
+import os
+from flask import Flask, request, jsonify
 from minio import Minio
 from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
                          BucketAlreadyExists)
 
-OBJECT_PATH = 'test_object.zip'
+app = Flask(__name__)
 
-# Initialize minioClient with an endpoint and keys.
-minioClient = Minio('localhost:9000',
-                    access_key='AKIAIOSFODNN7GRAINGER',
-                    secret_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYGRAINGERKEY',
-                    secure=False)
+@app.route('/minio', methods=['POST'])
+def box():
+    if request.method == 'POST':
+        response = {}
+        post_data = json.loads(request.form.get('data'))
+        metadata = post_data.get('metadata')
+        deposit_id = metadata['deposit_id']
+        file = request.files['file']
+        file.save(file_path)
 
-try:
-    print(minioClient.fput_object('3deposit', OBJECT_PATH, OBJECT_PATH))
-except ResponseError as err:
-    print(err)
+        OBJECT_PATH = deposit_id
+
+        # Initialize minioClient with an endpoint and keys.
+        minioClient = Minio('localhost:9000',
+                            access_key='AKIAIOSFODNN7GRAINGER',
+                            secret_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYGRAINGERKEY',
+                            secure=False)
+
+        try:
+            r = minioClient.fput_object('3deposit', OBJECT_PATH, OBJECT_PATH)
+            return jsonify({"etag": r})
+        except ResponseError as err:
+            return jsonify({"error": err})
+
+
+if __name__ == '__main__':
+    app.run()

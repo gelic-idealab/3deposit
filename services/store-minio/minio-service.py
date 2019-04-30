e07cee432e58ba0e67c778cf2af822398c990653
Wallace <rwwalla2@illinois.edu>
2019-04-30 15:43:47 -0500

    store through service works

--- services/store-minio/minio-service.py
+++ services/store-minio/minio-service.py
@@ -8,7 +8,7 @@ from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
 app = Flask(__name__)
 
 @app.route('/minio', methods=['POST'])
-def box():
+def minio():
     if request.method == 'POST':
         post_data = json.loads(request.form.get('data'))
         metadata = post_data.get('metadata')
@@ -17,15 +17,21 @@ def box():
         file.save(deposit_id)
 
         OBJECT_PATH = deposit_id
+        BUCKET_NAME = '3deposit'
 
         # Initialize minioClient with an endpoint and keys.
-        minioClient = Minio('localhost:9000',
+        minioClient = Minio('minio-server:9000',
                             access_key='AKIAIOSFODNN7GRAINGER',
                             secret_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYGRAINGERKEY',
                             secure=False)
 
         try:
-            r = minioClient.fput_object('3deposit', OBJECT_PATH, OBJECT_PATH)
+            minioClient.make_bucket(BUCKET_NAME)
+        except ResponseError as err:
+            return jsonify({"error": err})                    
+
+        try:
+            r = minioClient.fput_object(BUCKET_NAME, OBJECT_PATH, OBJECT_PATH)
             return jsonify({"etag": r})
         except ResponseError as err:
             return jsonify({"error": err})
--- services/store-minio/minio.sh
+++
@@ -1,6 +0,0 @@
-docker run -it -p 9000:9000 --name minio1 \
-  -v minio_data:/data \
-  -v minio_config:/root/.minio \
-  -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7GRAINGER" \
-  -e "MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYGRAINGERKEY" \
-  minio/minio server /data

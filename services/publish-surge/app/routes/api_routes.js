87398263f7381b34824accd5eae0ddcee19d20eb
Wallace <rwwalla2@illinois.edu>
2019-05-02 18:01:48 -0500

    async system call on post

--- services/publish-surge/app/routes/api_routes.js
+++ services/publish-surge/app/routes/api_routes.js
@@ -6,8 +6,16 @@ module.exports = function(app, db) {
         res.send(id);
     });
     app.post('/', (req, res) => {
-      // process post request
-      console.log(req.body)
-      res.send('post request working...')
+      // process post request with system call (to use surge)
+      const util = require('util');
+      const exec = util.promisify(require('child_process').exec);
+
+      async function ls() {
+          const { stdout, stderr } = await exec('ls ./tmp');
+          console.log('stdout:', stdout);
+          console.log('stderr:', stderr);
+          res.send({"stdout": stdout})
+        }
+      ls();
     });
   };

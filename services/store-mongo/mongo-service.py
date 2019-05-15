9030a72d168b099989d53853c8c2e81e5beee970
root <rwwalla2@illinois.edu>
2019-05-15 03:46:13 +0000

    mongo flask boilerplate

--- services/store-mongo/mongo-service.py
+++ services/store-mongo/mongo-service.py
@@ -1,14 +1,25 @@
 import os
 import json
 from flask import Flask, request, jsonify
-import pymogo
+from pymongo import MongoClient
 
 # https://api.mongodb.com/python/current/tutorial.html
 
 
+DATABASE_NAME = 'deposits'
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client[DATABASE_NAME]
+
 app = Flask(__name__)
 
-# app goes here
+@app.route('/db', methods=['GET', 'POST', 'DELETE'])
+def db():
+    try:
+        databases = client.list_database_names()
+        return jsonify({"databases": databases})
+    except Exception as err:
+        return jsonify({"err": err})
 
 
 if __name__ == '__main__':

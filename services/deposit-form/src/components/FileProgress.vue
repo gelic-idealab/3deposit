8dbaa790295b4cdae7941bb22d247b7916398a9d
Rob Wallace <rwwalla2@illinois.edu>
2019-05-16 20:39:57 -0500

    md5 hash on FileProgress

--- services/deposit-form/src/components/FileProgress.vue
+++ services/deposit-form/src/components/FileProgress.vue
@@ -10,7 +10,7 @@
           <div class="invalid-feedback">Example invalid custom file feedback</div>
         </div>
       <br>
-      <div class="progress">
+      <div class="progress mt-3 mb-3">
         <div class="progress-bar" role="progressbar" :style="{width: uploadPercentage+'%'}" aria-valuemin="0" aria-valuemax="100"></div>
       </div>
       <!-- <progress max="100" :value.prop="uploadPercentage"></progress> -->
@@ -22,29 +22,34 @@
 
 <script>
   import axios from 'axios'
-  import md5File from 'md5-file'
+  import CryptoJS from 'crypto-js'
 
 export default {
   data(){
     return {
       file: '',
       uploadPercentage: 0,
-      id: this.$parent.id
+      id: this.$parent.id,
+      checksum: this.$parent.checksum
 
     }
   },
   methods: {
-    hashFile(f) {
-      md5File(f, (err, hash)  => {
-        // eslint-disable-next-line
-        console.log(err);
-        return hash;
-      })
+    hashFile(file, callback) {
+      var fileReader = new FileReader();
+      fileReader.onload = function() {
+          var buff = fileReader.result;
+          var md5 = CryptoJS.MD5(buff).toString();
+          callback(md5);
+      };
+      fileReader.readAsArrayBuffer(file)
+    },
+    updateHash(md5) {
+      this.checksum = md5
     },
     handleFileUpload() {
       this.file = this.$refs.file.files[0];
-      // compute checksum here and attach to form component
-      this.checksum = this.hashFile(this.file);
+      this.hashFile(this.file, this.updateHash)
       this.submitFile();
     },
     submitFile() {

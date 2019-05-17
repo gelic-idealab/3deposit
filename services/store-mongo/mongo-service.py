5bc94ba6b0eed03d5275b1b31ac6ceaa1ec1c507
piehld <piehld@gmail.com>
2019-05-17 13:34:49 -0500

    Worked on Flask Mongo API; Posting metadata to Mongo; Getting metadata from Mongo.

--- services/store-mongo/mongo-service.py
+++ services/store-mongo/mongo-service.py
@@ -19,12 +19,37 @@ app = Flask(__name__)
 
 @app.route('/db', methods=['GET', 'POST', 'DELETE'])
 def db():
-    try:
-        databases = client.list_database_names()
-        return jsonify({"databases": databases})
-    except Exception as err:
-        return jsonify({"err": err})
+	try:
+		databases = client.list_database_names()
+		return jsonify({"databases": databases})
+	except Exception as err:
+		return jsonify({"err": err})
+
+
+@app.route('/object', methods=['GET', 'POST', 'DELETE'])
+def object():
+	if request.method == 'POST':
+		try:
+			post = json.loads(request.form.get('data')) # also can call it "metadata_obj"
+			db2 = client.MongoTest2
+			posts = db2.posts
+			post_id = posts.insert_one(post).inserted_id
+			# post_id
+			return jsonify({"post_id": str(post_id)})
+		except Exception as err:
+			return jsonify({"err": str(err)})
+
+	if request.method == 'GET':
+		try:
+			obj_data = json.loads(request.form.get('data'))
+			obj_id = obj_data.get('deposit_id')
+			db2 = client.MongoTest2
+			posts = db2.posts
+			metadata_obj = posts.find_one({"deposit_id":obj_id})
+			return jsonify({"metadata_obj": str(metadata_obj)})
+		except Exception as err:
+			return jsonify({"err": str(err)})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)

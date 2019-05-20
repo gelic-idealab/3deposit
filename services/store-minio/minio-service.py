ae0719f0f10bf2e95757455264e5a394530ea170
Joshi <mihirsj2@illinois.edu>
2019-05-20 17:54:41 -0500

    Bucket scoped GET endpoint fixed

--- services/store-minio/minio-service.py
+++ services/store-minio/minio-service.py
@@ -296,7 +296,6 @@ def create_app():
                         if d not in obj_names:
                             missing_ids.append(d)
 
-                            
                 else:
                     for obj in objects:
                         ret_object = {"metadata": str(obj.metadata), 

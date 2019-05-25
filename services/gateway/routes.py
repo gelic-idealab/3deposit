a67b6bf4e7ecbd00e5332681c0a8a97a3b192efa
Rob Wallace <rwwalla2@illinois.edu>
2019-05-25 17:37:30 -0500

    better route

--- services/gateway/routes.py
+++ services/gateway/routes.py
@@ -2,9 +2,12 @@ from views import index, login, logout, activeDepositForm
 
 
 def setup_routes(app):
+    # user routes
     app.router.add_get('/', index, name='index')
     app.router.add_get('/login', login, name='login')
     app.router.add_post('/login', login, name='login')
     app.router.add_get('/logout', logout, name='logout')
-    app.router.add_get('/activeDepositForm', activeDepositForm, name='activeDepositForm')
-    app.router.add_post('/activeDepositForm', activeDepositForm, name='activeDepositForm')
+
+    # deposit form routes
+    app.router.add_get('/frontend/forms/active', activeDepositForm)
+    app.router.add_post('/frontend/forms/active', activeDepositForm)

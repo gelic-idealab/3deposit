6797b925507949ba76067dbba8a0e6444ec1f6f0
piehld <piehld@gmail.com>
2019-05-31 11:51:50 -0500

    Added DocStrings to POST, GET, and DELETE request method processes.
    POST method now checks to make sure no object with the particular
    deposit_id already exists in the database or not.
    
    Additional code clean-up (removing redundant comments, adjusting line-lengths, etc.)

--- services/store-mongo/mongo-service.py
+++ services/store-mongo/mongo-service.py
@@ -35,6 +35,19 @@ def database():
 @app.route('/object', methods=['GET', 'POST', 'DELETE'])
 def object():
     if request.method == 'POST':
+        """ POST process for posting a metadata object to the Mongo Database.
+
+            Method [should later] receives service request containing "data"
+            and "config" keys, in the format of 'service_api.json'.
+
+            First checks database to make sure no objects with the given
+            deposit_id already exist, and if not, then posts it to the DB.
+
+            :return post_id: Unique id for posted object, assigned by MongoDB,
+                             returned as a string. Note that this ID is not
+                             the same as the 'deposit_id'.
+        """
+
         try:
             # config = json.loads(request.form.get('config'))
             # db_name = config.get('db_name')
@@ -42,13 +55,38 @@ def object():
             data = json.loads(request.form.get('data'))
             deposit_id = data.get('deposit_id')
             posts = db_name.posts
-            post_id = posts.insert_one(data).inserted_id
-            return jsonify({"post_id": str(post_id)})
+
+            # Make sure an object with the deposit ID doesn't already exist
+            if posts.find_one({"deposit_id":deposit_id}):
+                return jsonify({"err":("A metadata object with that deposit_id"
+                                        " already exists.") })
+            else:
+                post_id = posts.insert_one(data).inserted_id
+                return jsonify({"post_id": str(post_id)})
 
         except Exception as err:
             return jsonify({"err": str(err)})
 
+
     if request.method == 'GET':
+        """ GET process for retrieving a metadata object to the Mongo Database.
+
+            Method [should later] receives service request containing "data"
+            and "config" keys, in the format of 'service_api.json'.
+
+            Process first performs a "find all" on the database for objects
+            with the given 'deposit_id', and if there are more than one,
+            returns an error along with a list of all the metadata objects
+            with that particular deposit_id as well as the one that was most
+            recently added to the database. If only one exists, then that one
+            is returned.
+
+            :return metadata_obj: All data associated with the metadata object
+                                  with the specified 'deposit_id', returned as
+                                  a json object with the metadata as the value
+                                  and formatted as a string.
+        """
+
         try:
             # config = json.loads(request.form.get('config'))
             # db_name = config.get('db_name')
@@ -65,28 +103,44 @@ def object():
                     str('{"metadata_obj": ' + str(obj) + '}')
                     for obj in metadata_obj_iter_list ]
 
-                # Assign the most recently added 3D metadata object with the
-                # particular deposit_id of interest to "most_recent_metadata_obj"
                 most_recent_metadata_obj = metadata_obj_iter_list[-1]
 
                 return jsonify({
-                    "err":"You have more than one 3D metadata object with the \
-                      same deposit_id in your database! We are returning a list \
-                      of these objects back to you; however, you should consider \
-                      consolidating them...",
+                    "err":( "You have more than one 3D metadata object with "
+                            "the same deposit_id in your database! We are "
+                            "returning a list of these objects back to you; "
+                            "however, you should consolidate them." ),
                     "list_of_metadata_objs": list_of_metadata_objs,
                     "most_recent_metadata_obj": str(most_recent_metadata_obj)
                      })
 
             else:
-                metadata_obj = metadata_obj_iter_list[0]
-                # metadata_obj = posts.find_one({"deposit_id":deposit_id})
+                metadata_obj = posts.find_one({"deposit_id":deposit_id})
                 return jsonify({"metadata_obj": str(metadata_obj)})
 
         except Exception as err:
             return jsonify({"err": str(err)})
 
     if request.method == 'DELETE':
+        """ Process for deleting a metadata object to the Mongo Database.
+
+            Method [should later] receives service request containing "data"
+            and "config" keys, in the format of 'service_api.json'.
+
+            Process performs a "delete one" on the database for an object
+            with the given 'deposit_id'.
+
+            If there are >1 documents with the same deposit_id (for
+            instance, if one of them was inserted twice, creating a
+            duplicate entry), then just one of them is deleted (in
+            the same order as when each was added--i.e., the first
+            one that was added will be the first one that's deleted.
+
+            :return del_obj: pymongo.results.DeleteResult object for the
+                             deleted metadata object of the given 'deposit_id'
+                             (formatted as a string).
+        """
+
         try:
             # config = json.loads(request.form.get('config'))
             # db_name = config.get('db_name')
@@ -96,14 +150,8 @@ def object():
             posts = db_name.posts
             del_obj = posts.delete_one({"deposit_id": deposit_id})
 
-            # NOTE: If there are >1 documents with the same deposit_id
-            # (for instance, if one of them was inserted twice,
-            # creating a duplicate entry), then just one of them
-            # is deleted (in the same order as when each was added
-            # --i.e., the first one that was added will be the first
-            # one that's deleted.)
-
             return jsonify({"del_obj": str(del_obj)})
+
         except Exception as err:
             return jsonify({"err": str(err)})
 

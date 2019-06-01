caa4c63bbbc1d486854ccc285c3801a7d9769759
Rob Wallace <rwwalla2@illinois.edu>
2019-05-31 22:30:32 -0500

    resumable with progress

--- services/deposit-form/src/components/Upload.vue
+++ services/deposit-form/src/components/Upload.vue
@@ -5,10 +5,7 @@
         <button type="button" class="btn btn-primary m-3" aria-label="Add file" id="add-file-btn">
             <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add file
         </button>
-          <!-- <input type="file" class="custom-file-input" id="file" ref="file" v-on:change="chunkUpload()" required>
-          <label class="custom-file-label" for="file">Choose file...</label>
-          <div class="invalid-feedback">Example invalid custom file feedback</div> -->
-        <!-- </div> -->
+  
       <br>
       <div class="progress mt-3 mb-3">
         <div class="progress-bar" role="progressbar" :style="{width: uploadPercentage+'%'}" aria-valuemin="0" aria-valuemax="100"></div>
@@ -32,6 +29,7 @@ export default {
     }
   },
   mounted () {
+    let comp = this
     let r = new Resumable({ 
             target:'https://a84503bf-79a4-421a-81bc-20b00eaf5244.mock.pstmn.io/upload',
             chunkSize: 10*1024*1024, // 10MB
@@ -41,11 +39,10 @@ export default {
     r.assignBrowse(document.getElementById('add-file-btn'));
     r.on('fileAdded', function () {
         r.upload();
-        console.log(r)
-        r.on('progress', function() {
-            console.log(r.progress()*100)
-            })
-    })
+        r.on('progress', function () {
+          comp.uploadPercentage = r.progress()*100
+          })
+    });
   },
   props: {
     id: String

d809908bc9e7accfd72a2855cf614ff5067f29ab
Rob Wallace <rwwalla2@illinois.edu>
2019-06-18 15:33:17 -0500

    unused modules

--- services/gateway-old/threedeposit/admin/form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])

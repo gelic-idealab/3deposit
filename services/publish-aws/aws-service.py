4ed968bc9ec03aa136dcfb37c1c98b945a30f407
Rob Wallace <rwwalla2@illinois.edu>
2019-07-09 15:15:01 -0500

    using unpack

--- services/publish-aws/aws-service.py
+++ services/publish-aws/aws-service.py
@@ -11,30 +11,37 @@ import mimetypes
 import shutil
 from uuid import uuid4
 
-from keys import ACCESS_KEY, SECRET_KEY
-
+from unpack.unpack import get_value
 app = Flask(__name__) 
 
 @app.route('/', methods=['POST', 'GET', 'DELETE'])
 def index():
-    if request.method == 'POST':
-        try:
-            config = request.form.get('config')
-            data = request.form.get('data')
-            # use deposit id as directory name
-            TMP_DIR = data.get('deposit_id')
+    try:
+        # get keys from request object
+        access_key = get_value(request, 'config', 'access_key')
+        secret_key = get_value(request, 'config', 'secret_key')
+        print("KEYS:", access_key,  secret_key)
+
+        if request.method == 'POST':
+
+            # get deposit it and use as temporary directory name
+            deposit_id = get_value(request, 'data', 'deposit_id')
+            logging.debug(deposit_id)
+            # get AWS S3 bucket name being used for static hosting
+            bucket_name = get_value(request, 'config', 'bucket_name')
+            logging.debug(bucket_name)
             # get deposit file and save to temporary location
             deposit_file = request.files['file']
             deposit_file.save('tmp.zip')
             deposit_file.close()
 
             # Connect to S3
-            c = boto.connect_s3(ACCESS_KEY, SECRET_KEY)
-            b = c.get_bucket(BUCKET_NAME)
+            c = boto.connect_s3(access_key, secret_key)
+            b = c.get_bucket(bucket_name)
 
             # unzip into directory and remove tmp file
             with zipfile.ZipFile('tmp.zip','r') as zip_ref:
-                zip_ref.extractall(TMP_DIR)
+                zip_ref.extractall(deposit_id)
             os.remove('tmp.zip')
 
             file_paths = []
@@ -42,45 +49,58 @@ def index():
                 for dir_, _, files in os.walk(tmp_dir):
                     for file_name in files:
                         rel_dir = os.path.relpath(dir_, tmp_dir)
-                        rel_file = os.path.join(TMP_DIR, rel_dir, file_name)
+                        rel_file = os.path.join(tmp_dir, rel_dir, file_name)
                         file_paths.append(rel_file)
                 return file_paths
 
-            fps = get_file_paths(TMP_DIR)
+            fps = get_file_paths(deposit_id)
             print(fps)
 
 
             # Get file info
-            try: 
-                for f in fps:
-                    source_path = f
-                    source_size = os.stat(source_path).st_size
-                    print(source_path)
+            for f in fps:
+                source_path = f
+                source_size = os.stat(source_path).st_size
+                print(source_path)
 
-                    # Create a multipart upload request
-                    content_type = str(mimetypes.guess_type(source_path)[0])
-                    print(content_type)
-                    mp = b.initiate_multipart_upload(source_path, policy='public-read', headers = {'Content-Type': content_type})
+                # Create a multipart upload request
+                content_type = str(mimetypes.guess_type(source_path)[0])
+                mp = b.initiate_multipart_upload(source_path, policy='public-read', headers = {'Content-Type': content_type})
 
-                    # Use a chunk size of 50 MiB (feel free to change this)
-                    chunk_size = 52428800
-                    chunk_count = int(math.ceil(source_size / float(chunk_size)))
+                # Use a chunk size of 50 MiB (feel free to change this)
+                chunk_size = 52428800
+                chunk_count = int(math.ceil(source_size / float(chunk_size)))
 
-                    # Send the file parts, using FileChunkIO to create a file-like object
-                    # that points to a certain byte range within the original file. We
-                    # set bytes to never exceed the original file size.
-                    for i in range(chunk_count):
-                        offset = chunk_size * i
-                        bytes = min(chunk_size, source_size - offset)
-                        with FileChunkIO(source_path, 'r', offset=offset, bytes=bytes) as fp:
-                            mp.upload_part_from_file(fp, part_num=i + 1)
+                # Send the file parts, using FileChunkIO to create a file-like object
+                # that points to a certain byte range within the original file. We
+                # set bytes to never exceed the original file size.
+                for i in range(chunk_count):
+                    offset = chunk_size * i
+                    bytes = min(chunk_size, source_size - offset)
+                    with FileChunkIO(source_path, 'r', offset=offset, bytes=bytes) as fp:
+                        mp.upload_part_from_file(fp, part_num=i + 1)
 
-                        # Finish the upload
-                        mp.complete_upload()
-            finally:
-                if os.path.exists(TMP_DIR):
-                    shutil.rmtree(TMP_DIR)
-            return jsonify ({ 'uuid': TMP_DIR })
+                    # Finish the upload
+                    mp.complete_upload()
+
+            # walk until first html is found
+            for f in fps:
+                source_path = f
+                if source_path.endswith('.html'):
+                    index_path = source_path
+                    break   
+                                 
+            if os.path.exists(deposit_id):
+                shutil.rmtree(deposit_id)
+
+        # return deposit id as resource id and build object url from bucket name
+        # https://<bucket_name>.s3.amazonaws.com/<deposit_id>
+        return jsonify ({ 
+            'resource_id': deposit_id,
+            'location': f'https://{bucket_name}.s3.amazonaws.com/{index_path}'
+            })
+    except Exception as err:
+        return jsonify({ 'err': str(err)})
 
 if __name__ == '__main__':
     app.run()

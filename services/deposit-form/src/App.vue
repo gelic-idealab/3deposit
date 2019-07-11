d3a711ae745db45516832d035dd09661d6b64115
Rob Wallace <rwwalla2@illinois.edu>
2019-07-11 11:57:35 -0500

    get server domain

--- services/deposit-form/src/App.vue
+++ services/deposit-form/src/App.vue
@@ -36,7 +36,7 @@ export default {
   },
   mounted () {
     axios
-      .get('http://gateway.docker.localhost/form')
+      .get('http://api.3deposit.library.illinois.edu')
       .then(response => (this.form = response.data.form.content));
     // eslint-disable-next-line
     console.log(this.id);

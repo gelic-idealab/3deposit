cbc0958a438e3f42cbcb1d02ae1be1816949a094
Hsin-Yuan Wang <hywang3@illinois.edu>
2019-07-12 12:21:39 -0500

    added get and delete

--- services/publish-vimeo/vimeo-service.py
+++ services/publish-vimeo/vimeo-service.py
@@ -4,63 +4,38 @@ import requests
 import vimeo
 from flask import Flask, request, jsonify
 
-'''
-Flask app that takes in 360 video file, JSON data, and credentials 
-and publishes to Vimeo. 
-'''
-
 app = Flask(__name__)
 
-# def get_keys():
-#     try:
-#         config = json.loads(request.form.get('config'))
-#         auth = config.get('auth')
-#         access_token = auth.get('access_token')
-#         client_id = auth.get('client_id')          
-#         client_secret = auth.get('client_secret')
-#         channel_id = auth.get('channel_id')
-#         video_id = auth.get('video_id')
-#     except Exception as err:
-#         return jsonify({'err': str(err)})
+@app.route('/vimeo', methods=['POST', 'GET', 'DELETE'])
+def vimeo_service():
+    config = json.loads(request.form.get('config'))
+    auth = config.get('auth')
+    access_token = auth.get('access_token')
+    client_id = auth.get('client_id')          
+    client_secret = auth.get('client_secret')
+    client = vimeo.VimeoClient(
+        token=access_token,
+        key=client_id,
+        secret=client_secret
+    )
 
-@app.route('/vimeo', methods=['POST', 'GET'])
-def post_file():
     if request.method == 'POST':
-
-        config = json.loads(request.form.get('config'))
-        auth = config.get('auth')
-        access_token = auth.get('access_token')
-        client_id = auth.get('client_id')          
-        client_secret = auth.get('client_secret')
-        # channel_id = auth.get('channel_id')
-        # video_id = auth.get('video_id')
-        # construct request
-        client = vimeo.VimeoClient(
-            token=access_token,
-            key=client_id,
-            secret=client_secret
-        )
-
         try:
-            # unpack payload
             data = json.loads(request.form.get('data'))
             name = data.get('name')
             description = data.get('description')
             filename = data.get('filename')
-            # projection = data.get('projection')
-            # stereo_format = data.get('stereo_format')
-
+            projection = data.get('projection')
+            stereo_format = data.get('stereo_format')
             file = request.files.get('file')
             if file and filename:
                 file.save(filename)
-
                 uri = client.upload(filename, data={
                     'name': name,
                     'description': description,
-                    # 'spatial.projection': projection,
-                    # 'spatial.stereo_format': stereo_format
+                    'projection': projection,
+                    'stereo_format': stereo_format
                 })
-
                 os.remove(filename)
                 return jsonify({'res': {'uri': uri}})
             else:
@@ -68,44 +43,34 @@ def post_file():
         except Exception as err:
             return jsonify({'err': str(err)})
 
-def get_keys():
-    try:
-        config = json.loads(request.form.get('config'))
-        auth = config.get('auth')
-        access_token = auth.get('access_token')
-        client_id = auth.get('client_id')          
-        client_secret = auth.get('client_secret')
-        channel_id = auth.get('channel_id')
-        video_id = auth.get('video_id')
-    except Exception as err:
-        return jsonify({'err': str(err)})
+    elif request.method == 'GET':
+        try:
+            api = 'https://api.vimeo.com/'
+            data = json.loads(request.form.get('data'))
+            video_id = data.get('video_id')
+            get_video = client.get('{}videos/{}'.format(api, video_id))
+            get_comments = client.get('{}videos/{}/comments'.format(api, video_id))
+            if get_video and get_comments:
+                return jsonify({'res': {'video': get_video,
+                                        'comments': get_comments
+                        }})
+            else:
+                return jsonify({'err': 'No data provided'})
+        except Exception as err:
+            return jsonify({'err': str(err)})
 
-
-
-
-# def get_data():
-#     if request.method == 'GET':
-#         # construct request
-#         client = vimeo.VimeoClient(
-#             token=access_token,
-#             key=client_id,
-#             secret=client_secret
-#         )
-
-#         try:
-#             if:
-#                 videos = client.get('https://api.vimeo.com/channels/' + channel_id + '/videos')
-#                 followers = client.get('https://api.vimeo.com/channels/' + channel_id + '/users')
-#                 comments = client.get('https://api.vimeo.com/videos/' + video_id + '/comments')
-#                 return jsonify({'res': {
-#                                     'videos': videos,
-#                                     'followers': followers,
-#                                     'comments': comments
-#                         })
-#             else:
-#                 return jsonify({'err': 'No data provided'})
-#         except Exception as err:
-#             return jsonify({'err': str(err)})
+    elif request.method == 'DELETE':
+        try:
+            api = 'https://api.vimeo.com/'
+            data = json.loads(request.form.get('data'))
+            video_id = data.get('video_id')
+            del_video = client.delete('{}videos/{}'.format(api, video_id))
+            if del_video:
+                return jsonify({'res': 'Video deleted successfully'})
+            else:
+                return jsonify({'err': 'No video deleted'})
+        except Exception as err:
+            return jsonify({'err': str(err)})
 
 if __name__ == '__main__':
     app.run(debug=True)

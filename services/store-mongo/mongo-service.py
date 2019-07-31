593749f9aa7df9a5c8cc1b67a56d8bb62d2fac55
Rob Wallace <rwwalla2@illinois.edu>
2019-07-31 11:14:59 -0500

    use env for mongo server auth

--- services/store-mongo/mongo-service.py
+++ services/store-mongo/mongo-service.py
@@ -16,8 +16,8 @@ def create_client(request):
     if not request:
         return False
 
-    username = 'root'
-    password = 'example'
+    username = os.environ.get('MONGO_USERNAME')
+    password = os.environ.get('MONGO_PASSWORD')
 
     client = MongoClient(f'mongodb://{username}:{password}@mongo-server:27017/')
 

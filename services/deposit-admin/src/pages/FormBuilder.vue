0e08be313f396d41c27185aa0033e57db22368ca
Rob Wallace <rwwalla2@illinois.edu>
2019-08-02 17:01:17 -0500

    using vue-json-editor for FormBuilder

--- services/deposit-admin/src/pages/FormBuilder.vue
+++ services/deposit-admin/src/pages/FormBuilder.vue
@@ -1,23 +1,28 @@
 <template>
     <div>
-      <vue-json-pretty 
+        <vue-json-editor v-model="deposit_form" :show-btns="true"></vue-json-editor>
+      <!-- <vue-json-pretty 
         :data="deposit_form">
-      </vue-json-pretty>
+      </vue-json-pretty> -->
     </div>
 </template>
 
 <script>
 import axios from 'axios';
 import VueJsonPretty from 'vue-json-pretty';
+import vueJsonEditor from 'vue-json-editor'
 
 export default {
     data() {
         return {
-            deposit_form: {}
+            deposit_form: {
+                content: {}
+            }
         }
     },
     components: {
-        VueJsonPretty
+        VueJsonPretty,
+        vueJsonEditor
     },
     mounted() {
         axios.get('../api/form')

72a317b7426342bb3ff63fd609939b683dbb6759
Rob Wallace <rwwalla2@illinois.edu>
2019-08-07 11:42:31 -0500

    configure pg admin from .env

--- services/gateway/init_db.py
+++ services/gateway/init_db.py
@@ -13,8 +13,11 @@ from security import generate_password_hash
 DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
 
 ADMIN_DB_URL = DSN.format(
-    user='postgres', password='postgres', database='postgres',
-    host='postgres', port=5432
+    user=os.environ.get('PG_ADMIN_USERNAME'), 
+    password=os.environ.get('PG_ADMIN_PASSWORD'), 
+    database='postgres',
+    host='postgres', 
+    port=5432
 )
 
 admin_engine = create_engine(ADMIN_DB_URL, isolation_level='AUTOCOMMIT')
@@ -41,20 +44,20 @@ def setup_db(config):
     conn.close()
 
 
-def teardown_db(config):
+# def teardown_db(config):
 
-    db_name = config['database']
-    db_user = config['user']
+#     db_name = config['database']
+#     db_user = config['user']
 
-    conn = admin_engine.connect()
-    conn.execute("""
-      SELECT pg_terminate_backend(pg_stat_activity.pid)
-      FROM pg_stat_activity
-      WHERE pg_stat_activity.datname = '%s'
-        AND pid <> pg_backend_pid();""" % db_name)
-    conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
-    conn.execute("DROP ROLE IF EXISTS %s" % db_user)
-    conn.close()
+#     conn = admin_engine.connect()
+#     conn.execute("""
+#       SELECT pg_terminate_backend(pg_stat_activity.pid)
+#       FROM pg_stat_activity
+#       WHERE pg_stat_activity.datname = '%s'
+#         AND pid <> pg_backend_pid();""" % db_name)
+#     conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
+#     conn.execute("DROP ROLE IF EXISTS %s" % db_user)
+#     conn.close()
 
 
 def create_tables(engine):
@@ -62,14 +65,14 @@ def create_tables(engine):
     meta.create_all(bind=engine, tables=[forms, deposits, users, services, actions])
 
 
-def drop_tables(engine):
-    meta = MetaData()
-    meta.drop_all(bind=engine, tables=[forms, deposits, users, services, actions])
+# def drop_tables(engine):
+#     meta = MetaData()
+#     meta.drop_all(bind=engine, tables=[forms, deposits, users, services, actions])
 
 
 def create_admin(engine):
-    username = os.environ.get('PG_ADMIN_USERNAME')
-    password_hash = generate_password_hash(os.environ.get('PG_ADMIN_PASSWORD'))
+    username = os.environ.get('3DEPOSIT_ADMIN_USERNAME')
+    password_hash = generate_password_hash(os.environ.get('3DEPOSIT_ADMIN_PASSWORD'))
     role = 'admin'
     with engine.connect() as conn:
         conn.execute(users.insert().values(username=username, password_hash=password_hash, role=role))

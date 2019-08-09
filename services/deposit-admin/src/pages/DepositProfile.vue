f348adddbc63dc35ab3ca671846f81fb0da6511d
Rob Wallace <rwwalla2@illinois.edu>
2019-08-09 14:08:35 -0500

    file download uses deposit id as filename

--- services/deposit-admin/src/pages/DepositProfile.vue
+++ services/deposit-admin/src/pages/DepositProfile.vue
@@ -118,7 +118,7 @@ export default {
                 const url = window.URL.createObjectURL(new Blob([response.data]));
                 const link = document.createElement('a');
                 link.href = url;
-                link.setAttribute('download', 'file.zip'); //or any other extension
+                link.setAttribute('download', this.id+'.zip'); //or any other extension
                 document.body.appendChild(link);
                 link.click();
             });

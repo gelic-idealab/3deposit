1332b6247d6cb7afe69ab8f1b59e1e7064c47c67
Wallace <rwwalla2@illinois.edu>
2019-08-26 16:55:47 -0500

    clean up tmp files with finally block

--- services/publish-aws/aws-service.py
+++ services/publish-aws/aws-service.py
@@ -31,76 +31,80 @@ def index():
         b = c.get_bucket(bucket_name)
 
         if request.method == 'POST':
+            try:
+                # get deposit it and use as temporary directory name
+                deposit_id = get_value(request, 'data', 'deposit_id')
+                logging.debug(deposit_id)
 
-            # get deposit it and use as temporary directory name
-            deposit_id = get_value(request, 'data', 'deposit_id')
-            logging.debug(deposit_id)
+                # get deposit file and save to temporary location
+                deposit_file = request.files['file']
+                deposit_file.save('tmp.zip')
+                deposit_file.close()
 
-            # get deposit file and save to temporary location
-            deposit_file = request.files['file']
-            deposit_file.save('tmp.zip')
-            deposit_file.close()
+                # unzip into directory and remove tmp file
+                with zipfile.ZipFile('tmp.zip','r') as zip_ref:
+                    zip_ref.extractall(deposit_id)
 
-            # unzip into directory and remove tmp file
-            with zipfile.ZipFile('tmp.zip','r') as zip_ref:
-                zip_ref.extractall(deposit_id)
-            os.remove('tmp.zip')
+                file_paths = []
 
-            file_paths = []
+                def get_file_paths(tmp_dir):
+                    for dir_, _, files in os.walk(tmp_dir):
+                        for file_name in files:
+                            rel_dir = os.path.relpath(dir_, tmp_dir)
+                            rel_file = os.path.join(tmp_dir, rel_dir, file_name)
+                            file_paths.append(rel_file)
+                    return file_paths
 
-            def get_file_paths(tmp_dir):
-                for dir_, _, files in os.walk(tmp_dir):
-                    for file_name in files:
-                        rel_dir = os.path.relpath(dir_, tmp_dir)
-                        rel_file = os.path.join(tmp_dir, rel_dir, file_name)
-                        file_paths.append(rel_file)
-                return file_paths
+                fps = get_file_paths(deposit_id)
 
-            fps = get_file_paths(deposit_id)
-            print(fps)
+                # Get file info
+                for f in fps:
+                    source_path = f
+                    source_size = os.stat(source_path).st_size
+                    print(source_path)
 
-            # Get file info
-            for f in fps:
-                source_path = f
-                source_size = os.stat(source_path).st_size
-                print(source_path)
+                    # Create a multipart upload request
+                    content_type = str(mimetypes.guess_type(source_path)[0])
+                    mp = b.initiate_multipart_upload(source_path, policy='public-read', headers={'Content-Type': content_type})
 
-                # Create a multipart upload request
-                content_type = str(mimetypes.guess_type(source_path)[0])
-                mp = b.initiate_multipart_upload(source_path, policy='public-read', headers={'Content-Type': content_type})
+                    # Use a chunk size of 50 MiB (feel free to change this)
+                    chunk_size = 52428800
+                    chunk_count = int(math.ceil(source_size / float(chunk_size)))
 
-                # Use a chunk size of 50 MiB (feel free to change this)
-                chunk_size = 52428800
-                chunk_count = int(math.ceil(source_size / float(chunk_size)))
+                    # Send the file parts, using FileChunkIO to create a file-like object
+                    # that points to a certain byte range within the original file.
+                    # We set bytes to never exceed the original file size.
+                    for i in range(chunk_count):
+                        offset = chunk_size * i
+                        bytes = min(chunk_size, source_size - offset)
+                        with FileChunkIO(source_path, 'r', offset=offset, bytes=bytes) as fp:
+                            mp.upload_part_from_file(fp, part_num=i + 1)
 
-                # Send the file parts, using FileChunkIO to create a file-like object
-                # that points to a certain byte range within the original file.
-                # We set bytes to never exceed the original file size.
-                for i in range(chunk_count):
-                    offset = chunk_size * i
-                    bytes = min(chunk_size, source_size - offset)
-                    with FileChunkIO(source_path, 'r', offset=offset, bytes=bytes) as fp:
-                        mp.upload_part_from_file(fp, part_num=i + 1)
+                        # Finish the upload
+                        mp.complete_upload()
 
-                    # Finish the upload
-                    mp.complete_upload()
+                # walk until first html is found
+                for f in fps:
+                    source_path = f
+                    if source_path.endswith('.html'):
+                        index_path = source_path
+                        break
 
-            # walk until first html is found
-            for f in fps:
-                source_path = f
-                if source_path.endswith('.html'):
-                    index_path = source_path
-                    break
+                # return deposit id as resource id and build object url from bucket name
+                # https://<bucket_name>.s3.amazonaws.com/<deposit_id>
+                return jsonify({ 
+                    'resource_id': deposit_id,
+                    'location': f'https://{bucket_name}.s3.amazonaws.com/{index_path}'
+                })
+                
+            except Exception as err:
+                return jsonify({'err': str(err)})
 
-            if os.path.exists(deposit_id):
-                shutil.rmtree(deposit_id)
-
-            # return deposit id as resource id and build object url from bucket name
-            # https://<bucket_name>.s3.amazonaws.com/<deposit_id>
-            return jsonify({ 
-                'resource_id': deposit_id,
-                'location': f'https://{bucket_name}.s3.amazonaws.com/{index_path}'
-            })
+            finally:
+                if os.path.exists(deposit_id):
+                    shutil.rmtree(deposit_id)
+                if os.path.exists('tmp.zip'):
+                    os.remove('tmp.zip') 
 
         elif request.method == 'GET':
             resource_id = get_value(request, 'data', 'resource_id')
@@ -120,9 +124,6 @@ def index():
 
         elif request.method == 'DELETE':
             resource_id = get_value(request, 'data', 'resource_id')
-            # for obj in b:
-            #     if resource_id == obj:
-            #         obj.delete
 
             for key in b.list(prefix=resource_id):
                 key.delete()
@@ -131,7 +132,6 @@ def index():
 
     except Exception as err:
         return jsonify({'err': str(err)})
-
 
 if __name__ == '__main__':
     app.run()

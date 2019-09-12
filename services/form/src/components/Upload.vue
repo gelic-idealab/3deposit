b13169ce60758e3a8b2d945a85b3667ff32b11f8
Wallace <rwwalla2@illinois.edu>
2019-09-11 20:55:37 -0500

    adding pause and cancel to upload

--- services/form/src/components/Upload.vue
+++ services/form/src/components/Upload.vue
@@ -2,14 +2,25 @@
   <div class="container sticky-top" style="background-color: white">
     <div class="large-12 medium-12 small-12 cell">
 
-        <button type="button" class="btn btn-primary m-3" aria-label="Add file" id="add-file-btn">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add file
-        </button>
+      <b-button v-if="uploadPercentage == 0" size="lg" variant="primary" id="add-file-btn">
+        Add file
+      </b-button>
+      <template v-else-if="uploadPercentage < 100">
+        <b-button size="lg" variant="primary" id="uploading-file-btn">
+          <b-spinner></b-spinner>
+            {{ Math.round(uploadPercentage) }}%
+        </b-button>
+        <b-button size="lg" variant="warning" id="pause-upload-btn" @click="pauseUpload">Pause Upload</b-button>
+        <b-button size="lg" variant="danger" id="cancel-upload-btn" @click="cancelUpload">Cancel Upload</b-button>
+      </template>
+      <b-button v-else-if="uploadPercentage == 100" size="lg" variant="success" id="uploaded-file-btn">
+        Upload Successful
+      </b-button>
   
       <br>
-      <div class="progress mt-3 mb-3">
+      <!-- <div class="progress mt-3 mb-3">
         <div class="progress-bar" role="progressbar" :style="{width: uploadPercentage+'%'}" aria-valuemin="0" aria-valuemax="100"></div>
-      </div>
+      </div> -->
       <br>
     </div>
   </div>
@@ -25,13 +36,23 @@ export default {
   data(){
     return {
       uploadPercentage: 0,
-      checksum: ''
+      checksum: '',
+      r: {}
+    }
+  },
+  methods: {
+    pauseUpload () {
+      this.$emit('pause')
+    },
+    cancelUpload () {
+      this.$emit('cancel')
     }
   },
   mounted () {
-    let comp = this
+    let comp = this;
     let r = new Resumable({ 
             target:'../api/form/upload',
+            fileType: ['zip'],
             chunkSize: 64*1024*1024, // 64MB
             maxFileSize: 1000*10*1024*1024, // 10GB
             testChunks: false,
@@ -41,18 +62,24 @@ export default {
             });
     r.assignBrowse(document.getElementById('add-file-btn'));
     r.on('fileAdded', function () {
-        r.upload( function () {
-          // console.log('uploading chunk')
-        });
-        r.on('progress', function () {
-          comp.uploadPercentage = r.progress()*100
-          });
+      r.upload( function () {
+      });
+      r.on('progress', function () {
+        comp.uploadPercentage = r.progress()*100
+      });
     });
+    r.on('pause', function () {
+        r.pause( function () {
+          console.log("r.pause was called")
+        });
+    });
+    r.on('cancel', function () {
+      r.cancel();
+    });
+    comp.r = r;
   },
   props: {
     id: String
-  },
-  methods: {
-    }
+  }
 }
 </script>

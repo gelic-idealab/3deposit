42e38a3734b9ad623d85479b11c8b0adecf5b4d5
piehld <piehld@gmail.com>
2019-10-16 15:57:25 -0500

    Writing basic procedures for getting mesh metadata.

--- services/metadata/3d_model_metadata_testing/model/extract_model_metadata.py
+++ services/metadata/3d_model_metadata_testing/model/extract_model_metadata.py
@@ -8,7 +8,7 @@ import logging
 import zipfile
 import time
 import trimesh
-
+import numpy as np
 
 def main():
 
@@ -40,8 +40,8 @@ def main():
 
             # Can either check if extension is in supported type list,
             # OR simply TRY for all file to see if it can be loaded
-            # if file_metadata["ext"] in supported_3d_mesh_types:
-            #     mesh_metadata = get_3d_model_mesh_metadata(fn, file_metadata)
+            if file_metadata["ext"] in supported_3d_mesh_types:
+                mesh_metadata = get_3d_model_mesh_metadata(fn, file_metadata)
 
             # if file_metadata["ext"] == "obj":
             #     pass
@@ -57,8 +57,8 @@ def main():
         if gltf_metadata is not None:
             metadata_dict.update( {"GLTF metadata" : gltf_metadata} )
 
-        # if mesh_metadata is not None:
-        #     metadata_dict.update( {"Mesh metadata" : mesh_metadata} )
+        if mesh_metadata is not None:
+            metadata_dict.update( {"Mesh metadata" : mesh_metadata} )
 
         all_file_metadata.update( {fn : metadata_dict} )
 
@@ -116,9 +116,6 @@ def get_3d_model_file_metadata(model_file):
     except:
         return None
 
-    # for (index, track) in enumerate(m.tracks):
-    #     d = track.to_data()
-    #     md_dict.update( { index : d } )
 
 
 def get_3d_model_gltf_metadata(gltf_file, file_info_dict):
@@ -275,6 +272,37 @@ def get_3d_model_mesh_metadata(mesh_file, file_info_dict):
         identifier_md5              voxelized
         intersection
 
+        Get all attributes (non-methods) and remove any that are specialized objects that can't be interpreted:
+            Attributes to remove:
+                visual
+                ray
+                permutate
+                nearest
+                kdtree
+                face_adjacency_edges_tree
+                edges_sorted_tree
+                convex_hull
+                bounding_sphere
+                bounding_primitive
+                bounding_cylinder
+                bounding_box
+                bounding_box_oriented
+
+            This leaves us with the following attribute list to gather:
+            mesh_attr_list = ['area', 'area_faces', 'body_count', 'bounds', 'center_mass', 'centroid', 'density', 'edges',
+                              'edges_face', 'edges_sorted', 'edges_sparse', 'edges_unique', 'edges_unique_inverse', 'edges_unique_length',
+                              'euler_number', 'extents', 'face_adjacency', 'face_adjacency_angles', 'face_adjacency_convex',
+                              'face_adjacency_edges', 'face_adjacency_projections', 'face_adjacency_radius', 'face_adjacency_span',
+                              'face_adjacency_unshared', 'face_angles', 'face_angles_sparse','face_attributes', 'face_normals', 'faces',
+                              'faces_sparse', 'faces_unique_edges', 'facets', 'facets_area', 'facets_boundary', 'facets_normal',
+                              'facets_on_hull', 'facets_origin', 'identifier', 'identifier_md5', 'is_convex', 'is_empty', 'is_volume',
+                              'is_watertight', 'is_winding_consistent', 'mass', 'mass_properties', 'metadata', 'moment_inertia',
+                              'principal_inertia_components', 'principal_inertia_transform', 'principal_inertia_vectors',
+                              'referenced_vertices', 'scale', 'symmetry', 'symmetry_axis', 'symmetry_section', 'triangles',
+                              'triangles_center', 'triangles_cross', 'units', 'vertex_adjacency_graph', 'vertex_attributes',
+                              'vertex_defects', 'vertex_degree', 'vertex_faces', 'vertex_neighbors', 'vertex_normals', 'vertices', 'volume']
+
+
     """
 
     try:
@@ -285,9 +313,43 @@ def get_3d_model_mesh_metadata(mesh_file, file_info_dict):
 
     mesh_data_dict = {}
 
-    for a in dir(m):
-        if not a.startswith('__'):
-            mesh_data_dict.update( {a : getattr(a, d)})
+    def get_mesh_obj_attrs(mesh):
+        attr_dict = {}
+        for a in dir(mesh):
+            if not a.startswith('_'):  # and a in mesh_attr_list:
+                try:
+                    if not callable(getattr(mesh, a)):
+                        json.dumps( {a : getattr(mesh, a)} )   # Check to see if it can be serialized (e.g., not a numpy array or unfamiliar object)
+                        attr_dict.update( {a : getattr(mesh, a)} )
+                except:
+                    try:
+                        if type(getattr(mesh, a)) == np.ndarray: # If it's a numpy array, but a small one, keep it (e.g., 'centroid')
+                            if getattr(mesh, a).size <= 20:
+                                attr_dict.update( { a : str(getattr(mesh, a)) } )
+                        else:
+                            continue
+                    except:
+                        continue
+
+        return attr_dict
+
+    # if file_info_dict["ext"].lower() in ["obj", "stl"]:
+    try:
+        mesh_data_dict.update( get_mesh_obj_attrs(m) )
+    except:
+        pass
+
+    if file_info_dict["ext"].lower() in ["gltf", "glb"]:
+    # if 'is_watertight' not in mesh_data_dict.keys():
+        # Make a dump of the current mesh objects and analyze those
+        mesh_dump = m.dump()
+        for (index, obj) in enumerate(mesh_dump):
+            print(dir(obj))
+            try:
+                mesh_data_dict.update( { "mesh object "+str(index) : get_mesh_obj_attrs(obj) } )
+            except:
+                continue
+
 
     return mesh_data_dict
 

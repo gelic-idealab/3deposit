92874afd4c5879455d1c4082628adbd188581581
Wallace <rwwalla2@illinois.edu>
2019-10-25 17:41:37 -0500

    cleanup

--- services/gateway/init_db.py
+++ services/gateway/init_db.py
@@ -9,19 +9,6 @@ from db import forms, deposits, users, services, actions
 from security import generate_password_hash
 
 
-# DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
-
-# ADMIN_DB_URL = DSN.format(
-#     user='postgres',
-#     password=os.environ.get('PG_ADMIN_PASSWORD'), 
-#     database='postgres',
-#     host='postgres', 
-#     port=5432
-# )
-
-# admin_engine = create_engine(ADMIN_DB_URL, isolation_level='AUTOCOMMIT')
-
-
 ### Create user_engine from keys.env values
 user_username = os.environ.get('POSTGRES_USER')
 user_password = os.environ.get('POSTGRES_PASSWORD')
@@ -29,22 +16,6 @@ user_database = os.environ.get('POSTGRES_DB')
 user_host = 'postgres'
 user_port = '5432'
 user_engine = create_engine(f'postgresql://{user_username}:{user_password}@{user_host}:{user_port}/{user_database}')
-
-
-# def setup_db():
-
-#     db_name = user_database
-#     db_user = user_username
-#     db_pass = user_password
-
-#     conn = admin_engine.connect()
-#     conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
-#     conn.execute("DROP ROLE IF EXISTS %s" % db_user)
-#     conn.execute("CREATE USER %s WITH PASSWORD '%s'" % (db_user, db_pass))
-#     conn.execute("CREATE DATABASE %s ENCODING 'UTF8'" % db_name)
-#     conn.execute("GRANT ALL PRIVILEGES ON DATABASE %s TO %s" %
-#                  (db_name, db_user))
-#     conn.close()
 
 
 def create_tables(engine):

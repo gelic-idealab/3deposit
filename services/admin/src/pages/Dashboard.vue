fb815e46ff312d8d82c6057d87e8d5bf6d6e7c2a
Wallace <rwwalla2@illinois.edu>
2019-11-12 15:53:08 -0600

    fix p tag, col for stats cards

--- services/admin/src/pages/Dashboard.vue
+++ services/admin/src/pages/Dashboard.vue
@@ -3,14 +3,14 @@
 
     <!--Stats cards-->
     <div class="row">
-      <div class="col-md-6 col-xl-2" v-for="stats in statsCards" :key="stats.title">
+      <div class="col" v-for="stats in statsCards" :key="stats.title">
         <stats-card>
           <div class="icon-big text-center" :class="`icon-${stats.type}`" slot="header">
             <i :class="stats.icon"></i>
           </div>
           <div class="numbers" slot="content">
             <p>{{stats.title}}</p>
-            <p>{{stats.value}}</p>
+            {{stats.value}}
           </div>
           <div class="stats" slot="footer">
             <i :class="stats.footerIcon"></i> {{stats.footerText}}

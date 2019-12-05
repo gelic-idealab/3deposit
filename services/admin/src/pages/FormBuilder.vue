1f6ca4cdda285a575d6912c498c374e2b67c92db
Wallace <rwwalla2@illinois.edu>
2019-12-04 20:59:02 -0600

    load and save forms by id

--- services/admin/src/pages/FormBuilder.vue
+++ services/admin/src/pages/FormBuilder.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <input class="mr-3" type="text" v-model="deposit_form.form_id">Form ID
+        <p-button @click.native.prevent="getForm">Load Form</p-button>
+        <br>
         <div v-show="saveSuccess" class="alert alert-success">
             <button @click="saveSuccess = false" type="button" aria-hidden="true" class="close">×</button>
             <span>
@@ -7,39 +10,42 @@
         </div>
 
         <vue-json-editor v-model="deposit_form.content" :show-btns="true" @json-save="saveForm"></vue-json-editor>
-      <!-- <vue-json-pretty 
-        :data="deposit_form">
-      </vue-json-pretty> -->
 
     </div>
 </template>
 
 <script>
 import axios from 'axios';
-import VueJsonPretty from 'vue-json-pretty';
 import vueJsonEditor from 'vue-json-editor'
 
 export default {
     data() {
         return {
             deposit_form: {
-                form_id: 'deposit_form',
+                form_id: '',
                 content: {}
             },
             saveSuccess: false
         }
     },
     components: {
-        VueJsonPretty,
         vueJsonEditor
     },
     mounted() {
-        axios.get('../api/form', {params: {form_id: this.deposit_form.form_id}})
-        .then(response => {
-            this.deposit_form.content = response.data.form.content;
-        })
+
     },
     methods: {
+        getForm() {
+            axios.get('../api/form', {params: {form_id: this.deposit_form.form_id}})
+            .then(response => {
+                if (response.data.form) {
+                    this.deposit_form.content = response.data.form.content;
+                    console.log(response.data.form.form_id, 'loaded')
+                } else {
+                    console.log('form does not exist')
+                }
+            })
+        },
         saveForm() {
             axios.post('../api/form', this.deposit_form)
             .then(response => {

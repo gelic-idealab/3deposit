a3ef2e597484d36005ab32827507afa147fcd227
wallacerob <rwwalla2@illinois.edu>
2020-07-13 22:08:24 -0500

    start major rewrite: simplify frontend views, replace dashboard with custom vuetify; consolidate backend services, rewrite in go

---
+++ two/3deposit/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-content>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
